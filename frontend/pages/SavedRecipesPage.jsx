0b2188745a49947e7630180ca53575adb56ec9b4
KELECHIDAVIS <kboss16058@gmail.com>
2024-05-07 17:53:56 -0700

    Functionality for the saved Recipe page is done
    all that's left is Settings page and just adding back in the designs

--- frontend/pages/SavedRecipesPage.jsx
+++ frontend/pages/SavedRecipesPage.jsx
@@ -1,17 +1,60 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView,FlatList,  View, Text, Image , TouchableOpacity, StyleSheet, ActivityIndicator, Modal, Button} from 'react-native';
+import { SafeAreaView,FlatList,  View, Text, Image , TouchableOpacity, StyleSheet, ActivityIndicator, Modal, Button, ScrollView} from 'react-native';
 import { useDispatch } from 'react-redux';
 import { getSavedRecipes, setCurrentSavedRecipe } from '../features/recipes/recipeSlice';
 import { useSelector } from 'react-redux';
+import { AntDesign } from '@expo/vector-icons';
 import { useFocusEffect } from '@react-navigation/native';
 import SavedRecipeCard from '../components/SavedRecipeCard';
+import {openBrowserAsync} from 'expo-web-browser'
+const baseImageURL = "https://spoonacular.com/cdn/ingredients_100x100/"
+const returnModalContent= (currentRecipe) =>{
+  if(!currentRecipe){
+    return null; 
+  }
+  
+  return(
+    <ScrollView style={{padding:15, alignContent:'center'}}>
+          <Text style ={styles.title}>{currentRecipe.name}</Text>
+          <View style = {styles.timeContainer}>
+            <AntDesign name="clockcircle" size={18} color="black" />
+            <Text style={{fontSize: 18, fontWeight:'bold'}}>{currentRecipe.cookTime} mins</Text>
+          </View>
+          <Text style={{fontSize:18 , marginTop:8}}>Ingredients:</Text>
+          <FlatList 
+            horizontal
+            contentContainerStyle={{alignItems: 'center', justifyContent:'space-evenly'}}
+            data={currentRecipe.ingredients}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item})=>{
+              return(
+                <View style= {{ margin:10}}>
+                  <Image
+                    source={{ uri: baseImageURL + item.image }}
+                    style={{ width: 85, height: 85 , resizeMode:'stretch'}} // Adjust dimensions as needed
+                  />
+                  <Text style={{fontSize:14,backgroundColor:'tan'}}>{item.name}</Text>
+                </View>
+              )
+            }}
+          />
+          <Text style={{fontSize:18 , marginTop:8}}>Instructions:</Text>
+          {currentRecipe.instructions ? (
+            <Text style={{fontSize:16 , marginTop:8, alignSelf:'center'}}>{currentRecipe.instructions}</Text>
+          ) : (
+            <Button title= "Instructions Can Be Found Here" onPress={()=>openBrowserAsync(currentRecipe.sourceUrl)} /> 
+          )}
+          
+
+      </ScrollView>
+  )
+}
 function SavedRecipesPage({navigation}) {
 
   const dispatch = useDispatch();
   const {savedRecipes, isLoading} = useSelector((state)=>state.recipes)
   const [currentRecipe ,setCurrentRecipe] = useState(null); 
   const [isModalVisible, setModalVisible] = useState(false); 
-  const baseRecipeImageURL = "https://spoonacular.com/cdn/ingredients_100x100/"
   // Dispatch the function to get saved recipes when the component mounts or when navigation focus changes
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
@@ -26,6 +69,7 @@ function SavedRecipesPage({navigation}) {
     setCurrentRecipe(recipe); 
     setModalVisible(true); 
   }
+ 
   if(isLoading)
     {
       return(
@@ -49,10 +93,10 @@ function SavedRecipesPage({navigation}) {
           )
         }}
       />
-      <Modal visible={currentRecipe!==null}  >
+      <Modal visible={currentRecipe!== null}  >
         <SafeAreaView>
           <Button title="Close" onPress={()=> setCurrentRecipe(null)}/>
-          <Text>Saved Recipe Modal</Text>
+          {returnModalContent(currentRecipe)}
         </SafeAreaView>
       </Modal>
     </SafeAreaView>
