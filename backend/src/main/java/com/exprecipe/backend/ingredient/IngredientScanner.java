3874ceabf5419d16374e6bfbd73b2d991785f277
KELECHIDAVIS <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-02-16 13:59:30 -0800

    Google Cloud Installed Into Project

---
+++ backend/src/main/java/com/exprecipe/backend/ingredient/IngredientScanner.java
@@ -0,0 +1,10 @@
+package com.exprecipe.backend.ingredient;
+
+
+// Scans images and returns a comma separated list of detected ingredients
+public class IngredientScanner {
+    public IngredientScanner(){}
+
+
+
+}
