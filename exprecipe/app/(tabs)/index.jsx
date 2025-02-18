556af6a33fed4b3d6929794b9bc36bbef6bc5555
KELECHIDAVIS <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-02-17 23:17:58 -0800

    Able to search and get response
    Working on modal next

--- exprecipe/app/(tabs)/index.jsx
+++ exprecipe/app/(tabs)/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Text, View, SafeAreaView, StyleSheet, FlatList, Pressable, TextInput } from "react-native";
+import { Text, View, SafeAreaView, StyleSheet, FlatList, Pressable, TextInput, Modal } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from 'axios'
 import Ingredient from "../../components/ingredient";
@@ -11,9 +11,12 @@ export default function PantryPage() {
   const [loaded, setLoaded] = useState(false); // should only get ingredients on app launch, so only do if if not loaded
   const [ingredients, setIngredients] = useState([])
   const [user, setUser] = useState(null)
-  const [text, onChangeText] = useState("Enter Ingredient"); // for text input
+  const [text, onChangeText] = useState(""); // for text input
+  const [choiceOfIngrs, setChoiceOfIngrs] = useState([]); 
+  const [modalVisible, setModalVisible] = useState(false); 
   const apiUrl = process.env.EXPO_PUBLIC_API_URL
  
+  const ingrSearchAmt = 3; // amount of ingrs return after a recipe search
 
   //if this is the first time the user is opening the app, create a new user in the backend
   useEffect(()=>{
@@ -92,17 +95,67 @@ export default function PantryPage() {
     ); 
   } 
 
+  // saves the chosen ingredient to the backend
+  function addIngredient(chosenIngr){
+    const createIngr = async ()=>{
+      
+      if(!chosenIngr)
+      {
+        console.log("Trying to enter add ingredient method while chosenIngr is null. Returning..."); 
+        return
+      }
+      try {
+        // save ingredient to db
+        const response = await axios({
+          method:'post',
+          url:`${apiUrl}/${user.id}/ingredient`,
+          headers:{},
+          data:item  
+        }); 
+  
+        const data = response.data ; // returns ingredient as in our custom ingredient form
+
+        // add ingredient to ingredients list 
+        setIngredients([data, ...ingredients]); 
+
+        // after ingredient is added make chosen ingredient null and make sure modal is invisible
+
+        
+      } catch (error) {
+        console.log("Error Adding Ingredient: ",error)
+      }
+      setModalVisible(false); 
+    }
+
+    createIngr(); 
+  }
+
   // when plus is clicked, take name thats in text input then send through backend using ingredient search function
   function ingredientSearch (){
     // call backend with name 
     const searchIngredient = async ()=>{
       try{
-        const response = await axios.get(`${apiUrl}/${user.id}/ingredient/search?search=${text}&number=${3}`); 
+        const response = await axios.get(`${apiUrl}/${user.id}/ingredient/search?search=${text}&number=${ingrSearchAmt}`); 
   
-        console.log(response.data) // should be 4 ingredients
+        const data = response.data ; 
+
+
         // launch modal with ingredient choices
         // if just one just return that one
         // if none then throw up a message saying you couldn't find an ingredient w/ that name
+
+        if(data.length ==0 ){
+          console.log("Ingredient With That Name Was Not Found ") // make a popup that let's the user know there isn't an ingredient 
+          return; 
+        }else if (data.length ==1 ){ // just choose ingredient
+          addIngredient(data[0]); 
+        }else{
+          //launch choice modal which will then have the choice
+          setChoiceOfIngrs(data) // list of spIngredients
+          setModalVisible(true) // launch modal
+        }
+
+        // add chosen ingredient to ingredient list 
       }catch(error){
         console.log("Error searching that ingredient name: "+ error)
         console.log("url: ", `${apiUrl}/ingredient/search`)
@@ -115,20 +168,26 @@ export default function PantryPage() {
   }
   return (
     <SafeAreaView style= {styles.page}>
-      <View style={{padding:10, margin:10}}><Text>{"User "+ user.id}</Text></View>
+      <View style={{padding:10, margin:10}}><Text>{"User "+ user.id}</Text></View>  
+
       <View style={styles.subcontainer}>
+
+        {/*text input and button */}
         <View style={styles.inputContainer}>
-          <TextInput style={{backgroundColor:'white'}}
-            onChangeText={onChangeText}
+          <TextInput style={{backgroundColor:'white', width:150,paddingLeft:10 }}
+            onChangeText={newText=> onChangeText(newText)}
+            clearTextOnFocus={true}
             value={text}
           />
-          <Pressable onPress={ingredientSearch}><AntDesign name="plussquare" size={24} color="green" /></Pressable>
+          <Pressable onPress={ingredientSearch}><AntDesign name="plussquare" size={40} color="green" /></Pressable>
         </View>
         
-        <Pressable><MaterialCommunityIcons name="view-grid" size={24} color="grey" /></Pressable>
-        <Pressable><Feather name="list" size={24} color="green" /></Pressable>
+        {/* grid and list formatting*/}
+        <Pressable><MaterialCommunityIcons name="view-grid" size={40} color="grey" /></Pressable>
+        <Pressable><Feather name="list" size={40} color="green" /></Pressable>
       </View>
 
+      {/* ingredient list */}
       <View style={styles.subcontainer}>
         <FlatList 
         data = {ingredients}
@@ -136,8 +195,8 @@ export default function PantryPage() {
         keyExtractor={ingr => ingr.id} 
         />
       </View>
-
-
+      
+      
     </SafeAreaView>
 
   );
@@ -146,5 +205,5 @@ export default function PantryPage() {
 var styles = StyleSheet.create({
   page:{ flex: 1, alignItems: "center", margin:10,},
   subcontainer:{flexDirection:"row", alignContent:'center', justifyContent:'space-between', gap:14},
-  inputContainer:{flexDirection:"row"},
+  inputContainer:{flexDirection:"row",alignContent:'center',gap:4},
 })
