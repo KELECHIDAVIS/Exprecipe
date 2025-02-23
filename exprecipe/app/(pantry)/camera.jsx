7318810f3609a8f6882c07aa6c2bb0d20fd830e5
KELECHIDAVIS <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-02-22 16:05:07 -0800

    Taking Pictures and Displaying Works

--- exprecipe/app/(pantry)/camera.jsx
+++ exprecipe/app/(pantry)/camera.jsx
@@ -1,13 +1,14 @@
-import { View, Text, Pressable ,SafeAreaView,StyleSheet , Button} from "react-native";
+import { View, Text, Pressable ,SafeAreaView,StyleSheet , Button ,Image} from "react-native";
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useRouter } from "expo-router";
 
 export default function CameraScreen() {
   const [permission, requestPermission] = useCameraPermissions();
-
+  const ref = useRef(null); // camera's ref 
+  const [uri, setUri] = useState(null);
   const router = useRouter();
 
 
@@ -20,19 +21,44 @@ export default function CameraScreen() {
     // Camera permissions are not granted yet.
     return (
       <View style={styles.container}>
-        <Text style={styles.message}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+        <Button onPress={requestPermission} title="Grant Camera Permission" />
       </View>
     );
   }
 
+  const takePicture = async () => {
+    const photo = await ref.current?.takePictureAsync();
+    setUri(photo?.uri);
+  };
+
+  const renderCamera = ()=>{
+    return(
+      <CameraView style={styles.camera} ref={ref}>
+        <SafeAreaView style={{margin:5}}>
+          <Pressable><Text style={styles.text}> Cancel</Text></Pressable>
+        </SafeAreaView>
+        <Pressable onPress={takePicture} style={{backgroundColor:'white', width:85, height:80, borderRadius:50, position:'absolute', bottom:40, alignSelf:'center'}}      ></Pressable>
+      </CameraView>
+    )
+  }
+
+  const renderImage = ()=>{
+    console.log(uri); 
+    return(
+      <SafeAreaView style={{flex:1 , backgroundColor:'black',alignItems:'center', justifyContent:'center'}}>
+        <Image
+          source={{ uri }}
+          resizeMode="contain"
+          style={{ width: "80%",height:'80%',aspectRatio:1}}
+        />
+        <Button onPress={() => setUri(null)} title="Retake?" />
+      </SafeAreaView>
+    )
+  }
+  
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} >
-        <View style={styles.buttonContainer}>
-          <Pressable><Text>Hellooooooo</Text></Pressable>
-        </View>
-      </CameraView>
+      {uri? renderImage() : renderCamera()}
     </View>
   );
 }
@@ -66,5 +92,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
+    
   },
+  bottomContainer:{ width:'100%'  , alignSelf:'center', backgroundColor:'red', flexDirection:'row',  position:'absolute', bottom:40, alignContent:'center'}
 });
