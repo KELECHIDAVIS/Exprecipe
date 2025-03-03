7895de7ed8ec9fa940614bef9a03620bc314d7f1
KELECHIDAVIS <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-03-02 20:51:08 -0800

    Create SpoonacularRecipe.java

---
+++ backend/src/main/java/com/exprecipe/backend/recipe/SpoonacularRecipe.java
@@ -0,0 +1,23 @@
+package com.exprecipe.backend.recipe;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+// the form the api returns recipes in
+
+//@JsonIgnoreProperties
+//class Recipe {
+//    private int id;
+//    private String image;
+//    private String imageType;
+//    private int likes;
+//    private int missedIngredientCount;
+//    private List<Ingredient> missedIngredients;
+//    private String title;
+//    private List<Ingredient> unusedIngredients;
+//    private int usedIngredientCount;
+//    private List<Ingredient> usedIngredients;
+//
+//
+//}
