441d2d9af5986a1d09fcbf2f8d184ba43362987a
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-07-28 16:51:23 +0000

    return type

--- backend/src/main/java/com/exprecipe/backend/recipe/RecipeService.java
+++ backend/src/main/java/com/exprecipe/backend/recipe/RecipeService.java
@@ -13,11 +13,13 @@ import com.exprecipe.backend.user.userrecipe.UserRecipeRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -169,7 +171,7 @@ public class RecipeService {
     ranking: either 1 or 2: (1) returns recipes that use the most amt of our ingrs, (2) minimizes missing ingrs
     ignorePantry: if true it assumes you have common household ingredients like salt, flour, etc. false will just go off of inputted ingredients
      */
-    public ResponseEntity<String> getPossibleRecipes (Long userId, int numberOfRecipes, int ranking, boolean ignorePantry ) {
+    public ResponseEntity<List<RpdRecipeSearchByIngr>> getPossibleRecipes (Long userId, int numberOfRecipes, int ranking, boolean ignorePantry ) {
         Optional<User> possibleUser = userRepo.findById(userId);
         if(possibleUser.isPresent()) {
             Set<UserIngredient> ingrList = userIngredientRepo.findByUser_Id(userId);
@@ -193,10 +195,27 @@ public class RecipeService {
             headers.set("x-rapidapi-host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
 
             // make get call to rapid api
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> response = restTemplate.exchange(apiURL, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+            try {
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 
-            return response;
+                ResponseEntity<List<RpdRecipeSearchByIngr>> response = restTemplate.exchange(
+                        apiURL,
+                        HttpMethod.GET,
+                        new HttpEntity<>(headers),
+                        new ParameterizedTypeReference<List<RpdRecipeSearchByIngr>>() {}
+                );
+                List<RpdRecipeSearchByIngr> recipes = response.getBody();
+                return ResponseEntity.ok(recipes);
+
+            }catch(Exception e) {
+                e.printStackTrace();
+                System.out.println("Error when calling getPossibleRecipes:"+ e.getMessage());
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+
+
+
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
